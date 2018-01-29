a8c2eecc9c988a947a481fa482ac50feffae4fff
marekhuckmann <marek.hueckmann@gmail.com>
2018-01-29 15:21:59 +0100

    !!!

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my Angular Battlefield!';
+  title = 'my Angular Battlefield!!!';
 }
